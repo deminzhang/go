e8cb9d026cc8314c7ec6f775f668582e4d62a9df
demin_zhang <demin.zhang@kunlun-inc.com>
2021-04-19 09:29:25 +0800

    事件模块优化

--- src/common/event/event.go
+++ src/common/event/event.go
@@ -10,7 +10,9 @@ import (
 //
 type event struct {
 	when []interface{} //条件 按参数顺序,nil为无条件
-	call interface{}   //回调
+	// foo  interface{}   //原始方法
+	call reflect.Value //回调
+	argn int           //参数数
 }
 
 //事件响应组容器
@@ -19,7 +21,7 @@ var events = make(map[int][]event)
 //事件参数类型 以首次注册时为准
 var types = make(map[int]reflect.Type)
 
-var newWhen []interface{}
+var newWhen []interface{} //TODO 这个在并发情况下不安全,当然保证不动态Reg可没事
 
 //响应条件设置 仅对下一个Reg有效
 func When(cond ...interface{}) {
@@ -27,7 +29,8 @@ func When(cond ...interface{}) {
 	newWhen = cond
 }
 
-//注册响应(事件名,回调) 以首次注册时函数类型为准
+//Listener
+//注册响应(事件名,回调) 以首次注册时函数类型为准 尽量init时全注册好 动态注册可能造成call时的不确定性
 func Reg(id int, foo interface{}) {
 	t0 := types[id]
 	t := reflect.TypeOf(foo)
@@ -42,64 +45,96 @@ func Reg(id int, foo interface{}) {
 			log.Fatalln("Event.Reg type not equal:", id, t0, t)
 		}
 	}
-	list := events[id]
-	list = append(list, event{
+	call := reflect.ValueOf(foo)
+	list := append(events[id], event{
 		when: newWhen,
-		call: foo,
+		// foo:  foo,
+		call: call,
+		argn: call.Type().NumIn(),
 	})
 	events[id] = list
-
 	newWhen = nil
 }
 
-//触发事件_串行(事件名,参数集)
+//构建反射调用参数
+func makeArgv(a ...interface{}) ([]reflect.Value, int) {
+	argn := len(a)
+	in := make([]reflect.Value, argn)
+	for k, v := range a {
+		in[k] = reflect.ValueOf(v)
+	}
+	return in, argn
+}
+
+//筛选条件执行
+func unMatch(when []interface{}, a ...interface{}) bool {
+	if when != nil {
+		for k, v := range a {
+			if k < len(when) && when[k] != nil && when[k] != v {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+//Dispatcher
+//触发事件_同步串行(事件名,参数集)
 func Call(id int, a ...interface{}) {
 	list := events[id]
+	in, argn := makeArgv(a...)
 	for _, e := range list {
-		f := reflect.ValueOf(e.call)
-		if len(a) != f.Type().NumIn() {
-			log.Fatalln("Event.Call params number error:", id)
+		if argn != e.argn {
+			log.Fatalln("Event.Call params number error:", id, argn, e.argn)
 			return
 		}
-		in := make([]reflect.Value, len(a))
-		if e.when == nil {
-			for k, v := range a {
-				in[k] = reflect.ValueOf(v)
-			}
-		} else {
-			for k, v := range a {
-				in[k] = reflect.ValueOf(v)
-				if k < len(e.when) && e.when[k] != nil && e.when[k] != v {
-					return
-				}
-			}
+		if unMatch(e.when, a) {
+			continue
 		}
-		f.Call(in)
+		e.call.Call(in)
 	}
 }
 
-//触发事件_并行(事件名,参数集)确定互不冲突可用
+//触发事件_异步并行(事件名,参数集)确定互不冲突可用
 func GoCall(id int, a ...interface{}) {
 	list := events[id]
+	in, argn := makeArgv(a...)
 	for _, e := range list {
-		f := reflect.ValueOf(e.call)
-		if len(a) != f.Type().NumIn() {
-			log.Fatalln("Event.Call params number error:", id)
+		if argn != e.argn {
+			log.Fatalln("Event.GoCall params number error:", id, argn, e.argn)
 			return
 		}
-		in := make([]reflect.Value, len(a))
-		if e.when == nil {
-			for k, v := range a {
-				in[k] = reflect.ValueOf(v)
-			}
-		} else {
-			for k, v := range a {
-				in[k] = reflect.ValueOf(v)
-				if k < len(e.when) && e.when[k] != nil && e.when[k] != v {
-					return
-				}
-			}
+		if unMatch(e.when, a) {
+			continue
 		}
-		go f.Call(in)
+		go e.call.Call(in)
+	}
+}
+
+//触发事件_同步并行(事件名,参数集)确定互不冲突可用
+func GoCallS(id int, a ...interface{}) {
+	list := events[id]
+	num := len(list)
+	if num == 0 {
+		return
+	}
+	in, argn := makeArgv(a...)
+	for _, e := range list {
+		if argn != e.argn {
+			log.Fatalln("Event.GoCallS params number error:", id, argn, e.argn)
+			return
+		}
+		if unMatch(e.when, a) {
+			num--
+			continue
+		}
+		go func() {
+			defer func() {
+				num--
+			}()
+			e.call.Call(in)
+		}()
+	}
+	for num > 0 {
 	}
 }
